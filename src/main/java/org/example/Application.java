ca0114bb6adb4502df954ca6f37a5244e6e59e32
huangyanduo <1050510316@qq.com>
2023-02-08 09:44:45 +0800

    提供web接口验证功能

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,13 +2,21 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author huang
  */
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 }
